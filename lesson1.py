fb6e66fb03c66eb28f29c0064e1079a0264690af
frouglas <doug@ethree.com>
2016-01-06 11:01:31 -0800

    a first shot at writing in spyder and publishing to github
    trying it out

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# this is a file in which i'm learning the basics of NumPy, PANDAS, and GitHub
+
+import numpy as np
+
+x = np.zeros(3, dtype='3int8, float32, (2,3)float64')
+print(x)
